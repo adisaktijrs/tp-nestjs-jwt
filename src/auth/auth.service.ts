6a707ee9b529c01ca5190bca71006a8b2523bcba
adisaktijrs <adhisakti47@gmail.com>
2023-06-30 22:57:49 +0700

    feat: add signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signup() {
+  async signup(dto: AuthDto) {
+    const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
     return { message: 'signup was successfull' };
   }
+
   async signin() {
     return '';
   }
+
   async signout() {
     return '';
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    return hashedPassword;
   }
 }
